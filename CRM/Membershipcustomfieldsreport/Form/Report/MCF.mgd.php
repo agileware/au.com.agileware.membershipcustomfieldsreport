5691e5556e476160d94890440b80cc774a560c35
Justin Freeman <justin@agileware.com.au>
2017-08-15 07:26:02 +1000

    Initial commit

---
+++ CRM/Membershipcustomfieldsreport/Form/Report/MCF.mgd.php
@@ -0,0 +1,21 @@
+<?php
+// This file declares a managed database record of type "ReportTemplate".
+// The record will be automatically inserted, updated, or deleted from the
+// database as appropriate. For more details, see "hook_civicrm_managed" at:
+// http://wiki.civicrm.org/confluence/display/CRMDOC42/Hook+Reference
+return array (
+  0 => 
+  array (
+    'name' => 'CRM_Membershipcustomfieldsreport_Form_Report_MCF',
+    'entity' => 'ReportTemplate',
+    'params' => 
+    array (
+      'version' => 3,
+      'label' => 'Memberships with Contact Custom Fields Report',
+      'description' => 'Extended membership report with support of filtering/showing custom fields of Individual contact.',
+      'class_name' => 'CRM_Membershipcustomfieldsreport_Form_Report_MCF',
+      'report_url' => 'au.com.agileware.membershipcustomfieldsreport/MCF',
+      'component' => 'CiviMember',
+    ),
+  ),
+);
